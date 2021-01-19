f0c13bfcbf618d9081d6fc27017d34066a988472
juan moreno <juanluissv@MacBooks-MacBook-Pro.local>
2021-01-19 10:07:14 -0600

    Todo using React/Redux

---
+++ src/todos/TodoListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TodoListItem.css';
+
+const TodoListItem = ({ todo, onRemovePressed }) => (
+    <div className="todo-item-container">
+        <h3>{todo.text}</h3>
+        <div className="buttons-container">            
+            <button
+                onClick={() => onRemovePressed(todo.text)}
+                className="remove-button">Remove</button>
+        </div>
+    </div>
+);
+
+export default TodoListItem;
